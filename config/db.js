7534e0425790c2ed3fbf3f41f4e9321dae0de8eb
ViniSouza001 <viniciussouza130705@gmail.com>
2023-07-22 16:22:44 -0300

    exclusion

--- config/db.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV == "production") {
-    module.exports = {
-        mongoURI: "mongodb+srv://vinisouza:hamster007@cluster0.sqc5ntr.mongodb.net/?retryWrites=true&w=majority"
-    }
-} else {
-    module.exports = {
-        mongoURI: "mongodb://0.0.0.0:/blogapp"
-    }
-}
