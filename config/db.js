860396193c9a1675f4950ae81e58f2ce68a6c2c5
ViniSouza001 <viniciussouza130705@gmail.com>
2024-05-01 16:17:43 -0300

    updated db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
-if (process.env.NODE_ENV == "production") {
-  module.exports = {
-    mongoURI:
-      "mongodb+srv://viniciussouza130705:hamster007@cluster-blogapp.oqbjnvt.mongodb.net/?retryWrites=true&w=majority&appName=cluster-blogapp",
-  };
-} else {
-  module.exports = {
-    mongoURI: "mongodb://0.0.0.0:/blogapp",
-  };
-}
+// if (process.env.NODE_ENV == "production") {
+//   module.exports = {
+//     mongoURI:
+//       "mongodb+srv://viniciussouza130705:hamster007@cluster-blogapp.oqbjnvt.mongodb.net/?retryWrites=true&w=majority&appName=cluster-blogapp",
+//   };
+// } else {
+//   module.exports = {
+//     mongoURI: "mongodb://0.0.0.0:/blogapp",
+//   };
+// }
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://viniciussouza130705:hamster007@cluster-blogapp.oqbjnvt.mongodb.net/?retryWrites=true&w=majority&appName=cluster-blogapp",
+};
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,137 +1,158 @@
-const express = require("express")
-const router = express.Router()
-const mongoose = require('mongoose')
-require("../models/Usuario.js")
-const Usuario = mongoose.model("usuarios")
-const bcrypt = require("bcryptjs")
-const passport = require('passport')
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Usuario.js");
+const Usuario = mongoose.model("usuarios");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 router.get("/registro", (req, res) => {
-    if(req.user) {
-        req.flash("error_msg", "Para criar uma nova conta, você não pode estar logado")
-        res.redirect("/")
-    } else {
-        res.render("users/registro")
-    }
-})
+  if (req.user) {
+    req.flash(
+      "error_msg",
+      "Para criar uma nova conta, você não pode estar logado"
+    );
+    res.redirect("/");
+  } else {
+    res.render("users/registro");
+  }
+});
 
 router.post("/registro", (req, res) => {
-    const { nome, email, senha, senha2 } = req.body
-    var erros = []
+  const { nome, email, senha, senha2 } = req.body;
+  var erros = [];
 
-    if(!nome || typeof nome == undefined || nome == null) {
-        erros.push({texto: "Invalid name"})
-    }
+  if (!nome || typeof nome == undefined || nome == null) {
+    erros.push({ texto: "Invalid name" });
+  }
 
-    if(!email || typeof email == undefined || email == null) {
-        erros.push({texto: "Invalid E-mail"})
-    }
+  if (!email || typeof email == undefined || email == null) {
+    erros.push({ texto: "Invalid E-mail" });
+  }
 
-    if(!senha || typeof senha == undefined || senha == null) {
-        erros.push({texto: "Invalid password"})
-    }
+  if (!senha || typeof senha == undefined || senha == null) {
+    erros.push({ texto: "Invalid password" });
+  }
 
-    if(senha.length < 4) {
-        erros.push({texto: "Password too short"})
-    }
+  if (senha.length < 4) {
+    erros.push({ texto: "Password too short" });
+  }
 
-    if(senha != senha2) {
-        erros.push({texto: "Passwords do not match"})
-    }
+  if (senha != senha2) {
+    erros.push({ texto: "Passwords do not match" });
+  }
 
-    if(erros.length > 0) {
-        res.render("users/registro", {erros: erros})
-    } else {
-        Usuario.findOne({email: email}).lean().then(usuario => {
-            if(usuario) {
-                // já existe um usuário com email cadastrado
-                req.flash("error_msg", "An account with this email already exists in out system")
-                res.redirect("/usuarios/registro")
-            } else { 
-                // não existe um usuario com o email cadastrado
+  if (erros.length > 0) {
+    res.render("users/registro", { erros: erros });
+  } else {
+    Usuario.findOne({ email: email })
+      .lean()
+      .then((usuario) => {
+        if (usuario) {
+          // já existe um usuário com email cadastrado
+          req.flash(
+            "error_msg",
+            "An account with this email already exists in out system"
+          );
+          res.redirect("/usuarios/registro");
+        } else {
+          // não existe um usuario com o email cadastrado
 
-                if(email == "viniciusvieira130705@gmail.com" || email == "viniciussouza130705@gmail.com") {
-                    const novoUsuario = new Usuario({
-                        nome: nome,
-                        email: email,
-                        senha: senha,
-                        admin: 1
-                    })
-    
-                    bcrypt.genSalt(10, (erro, salt) => {
-                        bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
-                            if(erro) {
-                                req.flash("error_msg", "There was an error to save the user")
-                                res.redirect("/")
-                            }
-    
-                            novoUsuario.senha = hash
-    
-                            novoUsuario.save().then(() => {
-                                req.flash("success_msg", "User created successfully")
-                                res.redirect("/")
-                            }).catch(err => {
-                                res.flash("error_msg", "There was an internal error to create the user, try again")
-                                res.redirect("/usuarios/registro")
-                            })
-                        })
-                    })
-                } else {
-                    const novoUsuario = new Usuario({
-                        nome: nome,
-                        email: email,
-                        senha: senha
-                    })
-    
-                    bcrypt.genSalt(10, (erro, salt) => {
-                        bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
-                            if(erro) {
-                                req.flash("error_msg", "There was an error to save the user")
-                                res.redirect("/")
-                            }
-    
-                            novoUsuario.senha = hash
-    
-                            novoUsuario.save().then(() => {
-                                req.flash("success_msg", "User created successfully")
-                                res.redirect("/")
-                            }).catch(err => {
-                                res.flash("error_msg", "There was an internal error to create the user, try again")
-                                res.redirect("/usuarios/registro")
-                            })
-                        })
-                    })
+          if (email == "viniciusvieira130705@gmail.com") {
+            const novoUsuario = new Usuario({
+              nome: nome,
+              email: email,
+              senha: senha,
+              admin: 1,
+            });
+
+            bcrypt.genSalt(10, (erro, salt) => {
+              bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                if (erro) {
+                  req.flash("error_msg", "There was an error to save the user");
+                  res.redirect("/");
                 }
 
-            }
-        }).catch(err => {
-            req.flash("error_msg", "There was an internal error")
-        })
-    }
-})
+                novoUsuario.senha = hash;
+
+                novoUsuario
+                  .save()
+                  .then(() => {
+                    req.flash("success_msg", "User created successfully");
+                    res.redirect("/");
+                  })
+                  .catch((err) => {
+                    res.flash(
+                      "error_msg",
+                      "There was an internal error to create the user, try again"
+                    );
+                    res.redirect("/usuarios/registro");
+                  });
+              });
+            });
+          } else {
+            const novoUsuario = new Usuario({
+              nome: nome,
+              email: email,
+              senha: senha,
+            });
+
+            bcrypt.genSalt(10, (erro, salt) => {
+              bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                if (erro) {
+                  req.flash("error_msg", "There was an error to save the user");
+                  res.redirect("/");
+                }
+
+                novoUsuario.senha = hash;
+
+                novoUsuario
+                  .save()
+                  .then(() => {
+                    req.flash("success_msg", "User created successfully");
+                    res.redirect("/");
+                  })
+                  .catch((err) => {
+                    res.flash(
+                      "error_msg",
+                      "There was an internal error to create the user, try again"
+                    );
+                    res.redirect("/usuarios/registro");
+                  });
+              });
+            });
+          }
+        }
+      })
+      .catch((err) => {
+        req.flash("error_msg", "There was an internal error");
+      });
+  }
+});
 
 router.get("/login", (req, res) => {
-    if(req.user) {
-        req.flash("error_msg", "Você já está autenticado")
-        res.redirect("/")
-    } else res.render("users/login")
-})
+  if (req.user) {
+    req.flash("error_msg", "Você já está autenticado");
+    res.redirect("/");
+  } else res.render("users/login");
+});
 
 router.post("/login", (req, res, next) => {
-
-    passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/usuarios/login",
-        failureFlash: true
-    })(req, res, next)
-})
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/usuarios/login",
+    failureFlash: true,
+  })(req, res, next);
+});
 
 router.get("/logout", (req, res, next) => {
-    req.logout(function(err) {
-        if(err) {return next(err)}
-        req.flash("success_msg", "Deslogado com sucesso")
-        res.redirect("/")
-    })
-})
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success_msg", "Deslogado com sucesso");
+    res.redirect("/");
+  });
+});
 
-module.exports = router
+module.exports = router;
